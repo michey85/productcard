d597fc118b021dcd5e7f19616dd3579eed034561
Mikhail Nepomnyashchiy <nma@sessia.com>
2019-05-01 17:09:02 +0300

    proj created

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss')
+const cssnano = require('cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		},
+		port: 8000
+	});
+
+});
+gulp.task('css', function () {
+	var plugin = [
+		autoprefixer(),
+		cssnano()
+	];
+
+	return gulp.src('./css/style.css')
+		.pipe(postcss(plugin))
+		.pipe(gulp.dest('./cssmin'));
+});
+gulp.task('sass', function () {
+		return gulp.src('src/scss/**/*.scss')
+			.pipe(sass())
+			.pipe(gulp.dest('css'))
+			.pipe(browserSync.reload({stream:true}))
+	}
+);
+gulp.task('pug-compile', function () {
+	return gulp.src(['src/pug/**/*.pug', '!src/pug/includes/**/*.pug'])
+		.pipe(pug({pretty:true}))
+		.pipe(gulp.dest('./'))
+		.on('end', browserSync.reload);
+});
+
+gulp.task('watch',function () {
+	gulp.watch('src/pug/**/*.pug', gulp.series('pug-compile'));
+	gulp.watch('src/scss/**/*.scss', gulp.series('sass'))
+});
+
+gulp.task('default', gulp.series(
+	gulp.parallel('pug-compile', 'sass'),
+	gulp.parallel('watch', 'serve')
+));
